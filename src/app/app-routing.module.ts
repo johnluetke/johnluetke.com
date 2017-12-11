88d6e6aa144f8cdccc4675ad473c9a65ddb5c762
John Luetke <johnl1479@gmail.com>
2017-12-10 16:36:34 -0800

    Disable route debugging

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ResumeComponent}     from './resume/resume.component';
 
 const appRoutes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'portfolio', component: PortfolioComponent },
@@ -17,7 +18,7 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      { enableTracing: false } // <-- debugging purposes only
     )
   ],
   exports: [
