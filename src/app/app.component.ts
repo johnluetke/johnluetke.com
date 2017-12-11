6500687f74a4a0511102c721fadf7f53506500f3
John Luetke <johnl1479@gmail.com>
2017-12-10 16:34:50 -0800

    Colapse header when clicked elsewhere

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UiService } from './ui.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+constructor(private _uiService: UiService) { }
+
+  globalClick($event) {
+    if (this._uiService.isHeaderExpanded()) {
+      this._uiService.toggleHeader();
+    }
+  }
 }
